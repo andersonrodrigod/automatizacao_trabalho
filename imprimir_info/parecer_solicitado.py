959248b05b1c318aa329c133cd9e387ade93d66d
ANDERSON RODRIGO <99427400+andersonrodrigod@users.noreply.github.com>
2024-12-16 06:11:05 -0300

    asdasd

---
+++ imprimir_info/parecer_solicitado.py
@@ -0,0 +1,33 @@
+import pandas as pd
+ 
+df = pd.read_json("./dados/dados_coletados.json", encoding="utf-8")
+
+palavra_chave = "PARECER"
+
+info_assistente = df[df["info_assistente"].str.contains(palavra_chave, na=False)]
+
+parecer = []
+
+if not info_assistente.empty:
+    
+    for index, row in info_assistente.iterrows():
+        
+        solicitar_parecer = {
+            "codigo": row["codigo"],
+            "nome": row["nome"]
+        }
+
+        valores = list(solicitar_parecer.values())
+
+        if not valores in parecer:
+            parecer.append(valores)
+            
+    print(parecer)
+
+    with open("parecers_solicitados", "w", encoding="utf-8") as arquivo_txt:
+        for valor in parecer:
+            arquivo_txt.write(f"{valor[0]} - {valor[1]}\n")
+
+    
+else:
+    print("nenhum encontrado")
